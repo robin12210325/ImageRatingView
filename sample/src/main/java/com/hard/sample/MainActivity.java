7856f0ac1e06b05ed9dda6015ac9f57d1decb40b
qingtenglv <513629711@qq.com>
2016-12-29 22:53:05 +0800

    view complete

--- sample/src/main/java/com/hard/sample/MainActivity.java
+++ sample/src/main/java/com/hard/sample/MainActivity.java
@@ -7,18 +7,22 @@ import com.hard.imageratingview.ImageRatingView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageRatingView imageRatingView;
-
+    ImageRatingView sample0;
+    ImageRatingView sample1;
+    ImageRatingView sample2;
+    ImageRatingView sample3;
+    ImageRatingView sample4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageRatingView= (ImageRatingView) findViewById(R.id.image_rating_view);
-
-        imageRatingView.setRating(100);
-
+        sample0 = (ImageRatingView) findViewById(R.id.sample0);
+        sample1 = (ImageRatingView) findViewById(R.id.sample1);
+        sample2 = (ImageRatingView) findViewById(R.id.sample2);
+        sample3 = (ImageRatingView) findViewById(R.id.sample3);
+        sample4 = (ImageRatingView) findViewById(R.id.sample4);
 
     }
 }
